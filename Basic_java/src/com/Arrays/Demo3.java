0d2ac1e32c7f21392c3634b84ab32c5a3535040f
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-11-09 23:01:39 +0530

    Create Demo3.java

---
+++ Basic_java/src/com/Arrays/Demo3.java
@@ -0,0 +1,12 @@
+package com.Arrays;
+
+import java.util.Arrays;
+
+public class Demo3 {
+public static void main(String[] args) {
+	int[]a= {1,2,3,4};
+	System.out.println(Arrays.toString(a));
+	int[]b= {0,0,0,0};
+	
+}
+}
