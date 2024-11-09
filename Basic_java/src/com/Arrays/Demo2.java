0472b24d32a622b143cc651fcb7f78b344af190d
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-11-09 23:01:25 +0530

    Create Demo2.java

---
+++ Basic_java/src/com/Arrays/Demo2.java
@@ -0,0 +1,20 @@
+package com.Arrays;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		int[]a= {10,20,30,40};
+		m1(a);
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+
+		
+	}
+	static void m1( int[]a){
+		for(int i=0;i<a.length;i++) {
+			a[i]++;
+		}
+		System.out.println("m1 completed");
+	}
+}
