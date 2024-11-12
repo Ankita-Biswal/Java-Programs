33d06bf3ab1e2e14c125e22b91f8897b4230af49
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-11-12 18:51:42 +0530

    Binary Search

---
+++ Basic_java/src/com/searching/BinarySearch.java
@@ -0,0 +1,25 @@
+package com.searching;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		int[]a= {2,4,6,8,10,12,14};
+		System.out.println(search(a, 6));
+
+	}
+	
+
+	static int search(int[]a,int e) {
+		int start=0, end=a.length-1;
+		while(start<=end) {
+			int mid=(start+end)/2;
+			if(e==a[mid])return mid;
+			else if(e<a[mid]) 
+				end=mid-1;
+			else start=mid+1;
+				
+			
+		}
+		return -1;
+	}
+}
