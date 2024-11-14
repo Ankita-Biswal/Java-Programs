39c3a164c585bdef9c23fcd7354e51855ffd66fd
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-11-14 15:15:47 +0530

    commit

---
+++ Basic_java/src/com/string/NumtoWord.java
@@ -0,0 +1,27 @@
+package com.string;
+import java.util.*;
+public class NumtoWord {
+public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter one number");
+    int num=sc.nextInt();
+	numW(num/10000000," crore " );
+	numW((num/100000)%100," lakhs " );
+	numW((num/1000)%100," thousand " );
+	numW((num/100)%10," hundred " );
+	numW(num%100,"" );
+	
+}
+ public static void numW(int num,String s) {
+	 String[]one= {"","one","two","three","four","five","six","seven","eight","nine","ten","eleven"
+			       ,"twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+	 String[]two= {"","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninty"};
+	 
+	 if(num<=19)
+		 System.out.print(one[num]);
+	 else
+		 System.out.print(two[num/10]+""+one[num%10]);
+	 if(num!=0)
+		 System.out.print(s);
+ }
+}
