b08a3e4cfddd3cb2609e737fa3079aed604bb0e4
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-11-15 00:21:45 +0530

    sorting array

---
+++ Basic_java/src/com/Arrays/ArraySort.java
@@ -0,0 +1,13 @@
+package com.Arrays;
+
+import java.util.Arrays;
+
+public class ArraySort {
+
+	public static void main(String[] args) {
+		int[]a={9,4,1,7,5,2,3};
+		Arrays.sort(a);
+		System.out.println(Arrays.toString(a));
+	}
+
+}
