1b79ce7682562dbf2f3451868b5a12a49e376a8e
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-11-15 00:22:16 +0530

    bubble sort algorithm

---
+++ Basic_java/src/com/SortingAlgo/BubbleSort.java
@@ -0,0 +1,25 @@
+package com.SortingAlgo;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+public static void main(String[] args) {
+	int a[]={9,3,1,4,8,7};
+	System.out.println("Before sorting;"+Arrays.toString(a));
+	sort(a);
+	System.out.println("After sorting:"+Arrays.toString(a));
+}
+
+public static void sort(int[]a){
+	for(int i=0;i<a.length-1;i++ ) {
+		for (int j=0;j<a.length-1-i;j++) {
+			if(a[j]>a[j+1]) {
+				int temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+			
+		}
+	}
+}
+}
