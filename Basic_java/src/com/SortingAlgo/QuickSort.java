d3cb415c10f67b720d460023ff831af4dc924903
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-11-17 23:06:21 +0530

    quick sort

---
+++ Basic_java/src/com/SortingAlgo/QuickSort.java
@@ -0,0 +1,36 @@
+package com.SortingAlgo;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] a = {5, 8, 7, 4, 2, 1, 9, 6};
+        sort(a, 0, a.length - 1);
+        System.out.println(Arrays.toString(a));
+    }
+
+    static void sort(int[] a, int start, int end) {
+        if (start >= end) return;
+
+        int pivot = a[(start + end) / 2];
+        int i = start, j = end;
+
+        while (i <= j) {
+            while (a[i] < pivot) i++;
+            while (a[j] > pivot) j--;
+
+            if (i <= j) {
+                
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        sort(a, start, j);
+        sort(a, i, end);
+    }
+}
