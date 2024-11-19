97c2086b4145921e69ca1b48c8d166e09957cd35
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-11-19 23:38:20 +0530

    commit

--- Basic_java/src/com/LinkedList/Test.java
+++ Basic_java/src/com/LinkedList/Test.java
@@ -3,7 +3,13 @@ package com.LinkedList;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Node first=null;
+		first=new Node(10);
+		first.next=new Node(20);
+		first.next.next=new Node(30);
+		first.next.next.next=new Node(40);
+		first.next.next.next.next=new Node(50);
+		 Node curr=first;
 
 	}
 
