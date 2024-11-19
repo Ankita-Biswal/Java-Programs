f0d8aeaf8c9b9ec357096328c31c073ef85ff995
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-11-19 23:40:59 +0530

    commit

--- Basic_java/src/com/LinkedList/Node.java
+++ Basic_java/src/com/LinkedList/Node.java
@@ -4,5 +4,12 @@ public class Node {
    Object ele;
    Node next;
    
-	
+   public Node(Object e) {
+	   ele=e;
+	   next= null;
+   }
+   public Node(Object e,Node n) {
+	   ele=e;
+	   next=n;
+   }
 }
