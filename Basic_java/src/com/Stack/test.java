b4da57bcb57ee84e5369508838af980bdd7b630c
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-11-21 15:35:30 +0530

    commit

---
+++ Basic_java/src/com/Stack/test.java
@@ -0,0 +1,19 @@
+package com.Stack;
+
+public class test {
+
+	public static void main(String[] args) {
+		Stack_ArrayList a=new Stack_ArrayList();
+		
+		a.Push(10);
+		a.Push(20);
+		a.Push(30);
+		 a.Push(40);
+		 System.out.println(a.size());
+		 a.Pop();
+		 System.out.println(a.size());
+		 a.get();
+		 
+	}
+
+}
