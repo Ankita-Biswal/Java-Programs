3842106b85b1001518294027c029d64ab0f4d6b8
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-11-21 15:34:54 +0530

    commit

---
+++ Basic_java/src/com/LinkedList/DoubleLinkedList/User.java
@@ -0,0 +1,17 @@
+package com.LinkedList.DoubleLinkedList;
+
+public class User {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DoubleLinkedList d = new DoubleLinkedList();
+
+		d.add(20);
+		d.add(30);
+		d.add(40);
+		System.out.println(d.size()); 
+		d.traverse(); 
+
+	}
+
+}
