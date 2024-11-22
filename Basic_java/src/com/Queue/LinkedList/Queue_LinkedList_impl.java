da12a07810f6db7d3dcdb1c33fc86d0d864b1649
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-11-22 20:47:43 +0530

    commit

---
+++ Basic_java/src/com/Queue/LinkedList/Queue_LinkedList_impl.java
@@ -0,0 +1,20 @@
+package com.Queue.LinkedList;
+
+public class Queue_LinkedList_impl {
+ 
+	public static void main(String[] args) {
+		Queue_LinkedList l=new Queue_LinkedList();
+		l.Add(10);
+		l.Add(20);
+		l.Add(30);
+		l.get();
+		 l.poll();
+		 l.get();
+		 System.out.println(l.peek());
+		        //System.out.println(l.size());
+		       // System.out.println(l.peek());
+		        
+		        
+	}
+	
+}
