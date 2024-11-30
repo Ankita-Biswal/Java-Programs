8eb243655966e6d849382b62cf9e49f9115c2f9a
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-11-30 22:18:32 +0530

    commit

---
+++ Basic_java/src/com/comparatorinterface/Employee.java
@@ -0,0 +1,32 @@
+package com.comparatorinterface;
+
+public class Employee {
+    private String name;
+    double sal;
+
+    public Employee(String name, double sal) {
+        this.name = name;
+        this.sal = sal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSal() {
+        return sal;
+    }
+
+    public void setSal(double sal) {
+        this.sal = sal;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [name=" + name + ", sal=" + sal + "]";
+    }
+}
