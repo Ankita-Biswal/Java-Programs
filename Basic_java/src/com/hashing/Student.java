4f26e806deba64adb1e9f9ce19af4a32a4ebf95f
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-12-01 12:31:46 +0530

    update

--- Basic_java/src/com/hashing/Student.java
+++ Basic_java/src/com/hashing/Student.java
@@ -1,5 +1,23 @@
 package com.hashing;
 
 public class Student {
-
+  String name;
+  int marks;
+public Student(String n, int m) {
+	this.name = n;
+	this.marks = m;
+}
+@Override
+public String toString() {
+	return "Student [name=" + name + ", marks=" + marks + "]";
+}
+ 
+public int hashcode() {
+	return marks;
+}
+public boolean equals(Object arg) {
+	if(!(arg instanceof Student))return false;
+	Student s=(Student)arg;
+	return name.equals(s.name)&& marks==s.marks;
+}	
 }
