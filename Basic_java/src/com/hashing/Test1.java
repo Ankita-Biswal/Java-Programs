5a8b634f08c8e66a3d5c21a101bd629ba7ab1e28
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-12-01 12:02:24 +0530

    test the functionality

---
+++ Basic_java/src/com/hashing/Test1.java
@@ -0,0 +1,20 @@
+package com.hashing;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method s
+      Hashset s=new Hashset();
+      s.add(600);
+      s.add(700);
+      s.add(800);
+      s.add(900);
+      s.add(960);
+      s.add(980);
+      
+      System.out.println(s.size());
+     s.traverse();
+
+	}
+
+}
