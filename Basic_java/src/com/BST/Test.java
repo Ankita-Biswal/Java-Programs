937924437f184dbe2b383e026d66abc250168071
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-12-02 21:38:45 +0530

    updated test

--- Basic_java/src/com/BST/Test.java
+++ Basic_java/src/com/BST/Test.java
@@ -3,11 +3,14 @@ package com.BST;
 public class Test {
 
 	public static void main(String[] args) {
-		BinarySearchTree b= new BinarySearchTree();
+		BST b= new BST();
 		b.add(10);
 		b.add(20);
 		b.add(30);
 		b.add(40);
 		System.out.println(b.size());
+		b.add(50);
+		System.out.println(b.size());
 	}
 }
+
