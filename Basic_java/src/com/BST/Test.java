8de12539a0f9120b593a77f2c2c9f007b7e17c67
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-12-02 20:31:47 +0530

    commit

---
+++ Basic_java/src/com/BST/Test.java
@@ -0,0 +1,13 @@
+package com.BST;
+
+public class Test {
+
+	public static void main(String[] args) {
+		BinarySearchTree b= new BinarySearchTree();
+		b.add(10);
+		b.add(20);
+		b.add(30);
+		b.add(40);
+		System.out.println(b.size());
+	}
+}
