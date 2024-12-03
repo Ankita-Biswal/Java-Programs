c6f3e38e26286c8fb702d885c5e503d364374767
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-12-03 20:05:50 +0530

    collection framework methods

---
+++ Basic_java/src/com/Collection_frameWork/Test.java
@@ -0,0 +1,14 @@
+package com.Collection_frameWork;
+
+import java.util.ArrayList;
+
+public class Test {
+public static void main(String[] args) {
+	ArrayList a= new ArrayList();
+	System.out.println(a);
+	a.add(10);
+	a.add(20);
+	a.add(30);
+	System.out.println(a);
+}
+}
