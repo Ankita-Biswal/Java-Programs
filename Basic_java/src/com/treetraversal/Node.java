50ec653f4bca846b260413d70e2966a8711404f3
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-12-04 16:30:15 +0530

    commit

--- Basic_java/src/com/treetraversal/Node.java
+++ Basic_java/src/com/treetraversal/Node.java
@@ -1,5 +1,18 @@
 package com.treetraversal;
 
-public class Node {
 
+public class Node {
+	 Node left;
+	 Node right;
+	 int key;
+
+	public Node(Node left, Node right, int key) {
+		this.left = left;
+		this.right = right;
+		this.key = key;
+	}
+
+	public Node( int key) {
+		this.key = key;
+	}
 }
