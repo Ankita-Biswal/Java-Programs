d74701a465d74a38555925bb713057613d8a9882
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-12-06 21:17:51 +0530

    commit

--- Basic_java/src/com/Collection_frameWork/Test2.java
+++ Basic_java/src/com/Collection_frameWork/Test2.java
@@ -1,20 +1,24 @@
 package com.Collection_frameWork;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Test2 {
 public static void main(String[] args) {
 	ArrayList a= new ArrayList();
 	a.add(50);
-	a.add(60);
+    a.add(60);
 	a.add(70);
 	a.add(80);
 	System.out.println("a="+a);
 	ArrayList b= new ArrayList();
 	b.add(90);
-	b.add(100);
+    b.add(100);
 	System.out.println("b="+b);
 	b.addAll(a);
 	System.out.println("b="+b);
+	
+
+	
 }
 }
