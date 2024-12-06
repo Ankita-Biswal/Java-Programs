7ccaa8406d8bda67712dc42fb1ed82161d1042d8
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-12-06 21:18:04 +0530

    commit

---
+++ Basic_java/src/com/Collection_frameWork/Test10.java
@@ -0,0 +1,21 @@
+package com.Collection_frameWork;
+
+import java.util.HashSet;
+
+public class Test10 {
+
+	public static void main(String[] args) {
+		
+		HashSet h = new HashSet();
+		h.add(12);
+		h.add("Ankita");
+		h.add(10);
+		h.add(22);
+		h.add("Som");
+		h.add("Simpul");
+		h.add(35);
+		h.add("Ankita");
+		System.out.println("h="+h);
+	}
+
+}
