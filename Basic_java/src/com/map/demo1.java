636004ddb0241694c48b6e311c789158e6dca077
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-12-09 20:30:14 +0530

    commit

--- Basic_java/src/com/map/demo1.java
+++ Basic_java/src/com/map/demo1.java
@@ -12,6 +12,12 @@ public static void main(String[] args) {
 	map1.put(104, "Rintu");
 	map1.put(105, "Chintu");
 	System.out.println(map1);
+	Map map2=new HashMap();
+	map2.put("abc",1011);
+	map2.put("xyz",1022);
+	System.out.println(map2);
 	
+	map1.putAll(map2);
+	System.out.println(map1);
 }
 }
