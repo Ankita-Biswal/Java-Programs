d38e97b338da2de38412776e82d5a667bede66ee
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-12-09 20:26:33 +0530

    implementing map methods for java

---
+++ Basic_java/src/com/map/demo1.java
@@ -0,0 +1,17 @@
+package com.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class demo1 {
+public static void main(String[] args) {
+	Map map1=new HashMap();
+	map1.put(101, "Ankita");
+	map1.put(102, "Soumitra");
+	map1.put(103, "Ruchi");
+	map1.put(104, "Rintu");
+	map1.put(105, "Chintu");
+	System.out.println(map1);
+	
+}
+}
