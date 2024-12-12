330f0587522c5d911ec5500902f0fdecdfa05401
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-12-12 19:42:51 +0530

    program using typecasting

---
+++ Basic_java/src/com/typecasting/widening/budget.java
@@ -0,0 +1,12 @@
+package com.typecasting.widening;
+
+public class budget {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int budget=20000;
+       double modify=budget;
+       System.out.println(modify);
+	}
+
+}
