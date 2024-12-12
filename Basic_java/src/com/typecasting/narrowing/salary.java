69db877bdfcf17b7ce525fb3189c83d9d7a8c393
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-12-12 19:42:37 +0530

    commit

---
+++ Basic_java/src/com/typecasting/narrowing/salary.java
@@ -0,0 +1,12 @@
+package com.typecasting.narrowing;
+
+public class salary {
+
+	public static void main(String[] args) {
+		double sal=10000.008;
+		int sal1=(int)sal;
+		System.out.println(sal1);
+
+	}
+
+}
