910e4d77fc55a02fc6a629de55f66c66cccc37d2
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-12-21 14:52:21 +0530

    predicate interface for lamda expression

---
+++ Basic_java/src/com/lamdaexp/example1.java
@@ -0,0 +1,19 @@
+package com.lamdaexp;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		List<Integer> list=Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		
+		Predicate<Integer> p= i-> i%2==0;
+		
+		for(int n:list) {
+			if(p.test(n))
+			System.out.println(n);
+		}
+	}
+}
