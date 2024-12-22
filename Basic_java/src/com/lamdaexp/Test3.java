ead03e24b390e39e30fad59ae1a28c56974c9392
Ankita-Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-12-22 11:31:14 +0530

    commit

---
+++ Basic_java/src/com/lamdaexp/Test3.java
@@ -0,0 +1,15 @@
+package com.lamdaexp;
+
+
+interface I3{
+	int Square(int i);
+}
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     I3 i3=a->a*a;
+     System.out.println(i3.Square(30));
+	}
+
+}
